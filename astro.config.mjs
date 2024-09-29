ca1fa5eaf6d6ba40f0dcd84f4622a89d1db04a98
JoeriKaiser <joeri.kaiser@hotmail.com>
2024-09-29 16:26:31 +0200

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import mdx from "@astrojs/mdx";
+
+export default defineConfig({
+  integrations: [mdx()],
+});
---
+++ src/utils/viewTransitions.ts
@@ -0,0 +1,40 @@
+import type { TransitionAnimationPair } from "astro";
+
+const forwards: TransitionAnimationPair = {
+  old: {
+    name: "fadeOutUp",
+    duration: "0.2s",
+    easing: "ease-in",
+    fillMode: "forwards",
+  },
+  new: {
+    name: "fadeInUp",
+    duration: "0.3s",
+    delay: "0.2s",
+    easing: "ease-out",
+    fillMode: "backwards",
+  },
+};
+
+const backwards: TransitionAnimationPair = {
+  old: {
+    name: "fadeOutDown",
+    duration: "0.2s",
+    easing: "ease-in",
+    fillMode: "forwards",
+  },
+  new: {
+    name: "fadeInDown",
+    duration: "0.3s",
+    delay: "0.2s",
+    easing: "ease-out",
+    fillMode: "backwards",
+  },
+};
+
+const viewTransition = {
+  forwards,
+  backwards,
+};
+
+export default viewTransition;
