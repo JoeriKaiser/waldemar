57b0e41c769f13e03987d24ac41d6251833819a4
JoeriKaiser <joeri.kaiser@hotmail.com>
2024-09-29 16:29:19 +0200

    Configured vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
+import vercel from "@astrojs/vercel/serverless";
+
 export default defineConfig({
   integrations: [mdx()],
+  output: "server",
+  adapter: vercel(),
 });
