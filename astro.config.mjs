aa07ce90465c1ec2d91b86033daac0b5302d06d7
joeri <joeri.kaiser@hotmail.com>
2025-06-18 23:37:57 +0200

    replaced eslint, prettier and stylelint with biomw

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
+import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
-import mdx from "@astrojs/mdx";
-
-import vercel from "@astrojs/vercel/serverless";
-
 export default defineConfig({
-  integrations: [mdx()],
-  output: "server",
-  adapter: vercel(),
+	integrations: [mdx()],
+	output: "server",
+	adapter: vercel(),
 });
--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 
 export function formatDate(date: Date) {
-  console.log(date);
-  return DateTime.fromJSDate(date).toFormat("MMMM yyyy");
+	console.log(date);
+	return DateTime.fromJSDate(date).toFormat("MMMM yyyy");
 }
