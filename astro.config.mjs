de528ac93e1e043ade07e25b20f3180c6089b9d6
JoeriKaiser <joeri.kaiser@hotmail.com>
2025-11-09 19:16:31 +0100

    chore: refactor to tailwind styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,14 @@ import mdx from "@astrojs/mdx";
 
 import vercel from "@astrojs/vercel";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   integrations: [mdx()],
   output: "server",
   adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
